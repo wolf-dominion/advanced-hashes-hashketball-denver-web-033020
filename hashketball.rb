a541a9135e1870e02497a0c119c85c86a661abd4
Hannah Drury <hannah_drury@yahoo.com>
2020-03-17 22:38:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -207,7 +207,7 @@ def big_shoe_rebounds()
   data.each do |location|
     location[1][:players].each do |n|
       if shoesize < n[:shoe]
-        shoesize = location[1][:players][0][:shoe]
+        shoesize = n[:shoe]
       end
     puts "Shoe size is #{shoesize}"
   end
